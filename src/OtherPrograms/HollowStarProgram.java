7a3406a1c00bff058c099716a98bb072f3fe46c0
SathyaVN <v.n.sathyasri@gmail.com>
2022-05-12 18:31:26 +0530

    Star pattern program
    To print hollow star pattern

---
+++ src/OtherPrograms/HollowStarProgram.java
@@ -0,0 +1,20 @@
+package OtherPrograms;
+
+public class HollowStarProgram {
+    public static void main(String args[]){
+        for(int i=1;i<5;i++){
+
+            for(int j=1;j<=5;j++){
+                if((i==2&j==2)|(i==2&j==3)|(i==2&j==4)|(i==3&j==2)|(i==3&j==3)|(i==3&j==4))
+                    System.out.print(" ");
+                else {
+                    System.out.print("*");
+                }
+
+
+            }
+            System.out.println();
+
+        }
+    }
+}
