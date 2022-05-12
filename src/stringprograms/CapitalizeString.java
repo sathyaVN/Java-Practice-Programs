8f9420e2025628171da631c38ce53a9b31bd51f0
SathyaVN <v.n.sathyasri@gmail.com>
2022-05-12 12:58:16 +0530

    commit with changes

--- src/stringprograms/CapitalizeString.java
+++ src/stringprograms/CapitalizeString.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class CapitalizeString {
+package stringprograms;
+
+import java.util.Scanner;
+
+public class CapitalizeString {
+    public static void main(String[] args) {
+        Scanner scantext = new Scanner(System.in);
+        System.out.println("Enter the String to capitalize each word::");
+        String inputSentence = scantext.nextLine();
+
+        String[] sentenceArray = inputSentence.split(" ");
+        System.out.println("Total number of words in a given sentence::" + sentenceArray.length);
+
+        for (int i = 0; i < sentenceArray.length; i++) {
+            String outputString = sentenceArray[i].substring(0, 1).toUpperCase() + sentenceArray[i].substring(1, sentenceArray[i].length());
+            System.out.print(outputString);
+            System.out.print(" ");
+        }
+    }
 }
